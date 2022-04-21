fc78a3e8922c4cce63ca0c6a01bdfd415284f0e1
Willow Carlson-Huber <willow.ch@protonmail.com>
2022-04-20 19:25:40 -0500

    fixed sound

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 const fs = require('fs');
 const { DexcomApiClient } = require('@diakem/dexcom-api-client');
-var play = require('play').Play();
+var player = require('play-sound')(opts = {})
 var alert = require('alert-node');
-require('dotenv').config()
-
-// Check whether the sound file exists.
-if (!fs.existsSync("./noise.mp3")) {
-	console.error("write a loud noise to noise.mp3");
-	process.exit(1);
-}
-
-if (!process.env.USERNAME || !process.env.PASSWORD) {
-	console.error("set USERNAME and PASSWORD environment variables");
-	process.exit(1);
-}
-
-const LOW = process.env.LOW || 50;
-const HIGH = process.env.HIGH || 250;
-
-const { read } = DexcomApiClient({
-	username: process.env.USERNAME,
-	password: process.env.PASSWORD,
-	server: "US",
-});
-play.on('error', () => {
-	console.error("error playing sound");
-	alert("YOUR SOUND IS BROKEN! PLEASE FIX IT!");
-})
+require('dotenv').config();
 (async () => {
+
+	// Check whether the sound file exists.
+	if (!fs.existsSync("./noise.wav")) {
+		console.error("write a loud noise to noise.wav");
+		process.exit(1);
+	}
+
+	if (!process.env.USERNAME || !process.env.PASSWORD) {
+		console.error("set USERNAME and PASSWORD environment variables");
+		process.exit(1);
+	}
+
+	const LOW = process.env.LOW || 50;
+	const HIGH = process.env.HIGH || 250;
+
+	const { read } = DexcomApiClient({
+		username: process.env.USERNAME,
+		password: process.env.PASSWORD,
+		server: "US",
+	});
+	
 	while (true) {
 		let data = await read();
 		let last_data = data[data.length - 1];
 		console.log(`${last_data.value}`)
 		if (last_data.value < LOW || last_data.value > HIGH) {
-			play.sound('./noise.mp3');
+			player.play('./noise.wav', function (err) { alert("YOUR SOUND IS BROKEN PROBABLY?") });
 		}
 
 		// wait five minutes before checking again
